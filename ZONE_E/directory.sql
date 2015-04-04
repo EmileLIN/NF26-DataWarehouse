5bccdff6a3e1996e089b99c9eff761e036c48500
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-04-04 11:29:24 +0200

    Directories builder
    This file allows us to build directory pointers in DB.
    One hand,we can point at the source directory to get source data, On
    the other hand, we can build our bad file and log file to trace the
    process.

---
+++ ZONE_E/directory.sql
@@ -0,0 +1,8 @@
+--Create Directories
+
+
+CREATE OR REPLACE DIRECTORY Fantastic_src_directory AS '/home/nf26/data/';
+CREATE OR REPLACE DIRECTORY Fantastic_log_directory AS '/volsme/users/nf26p018/test/';
+
+CREATE OR REPLACE DIRECTORY Fantastic_src_depart_dir AS '/volsme/users/nf26p018/data/';
+CREATE OR REPLACE DIRECTORY Fantastic_log_depart_dir AS '/volsme/users/nf26p018/tmp/';
