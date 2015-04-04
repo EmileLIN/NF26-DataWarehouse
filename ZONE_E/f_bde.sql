7de8b172b6d6db7bc2e9f5d9d158f75bb7168273
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-04-04 11:26:24 +0200

    main file for the zone E
    This is the main file for the zone of extraction, you could build zone
    E with this file .

---
+++ ZONE_E/f_bde.sql
@@ -0,0 +1,14 @@
+--Create the source and log directories
+@directory.sql;
+
+--Create View f_bde_catalogue 
+@f_bde_catalogue.sql;
+
+--Create Table f_bde_marketing
+@f_bde_marketing.sql;
+
+--Create Table f_bde_departement
+@f_bde_departement.sql;
+
+--Create Table f_bde_vente.sql
+@f_bde_vente.sql;
