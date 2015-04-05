038d84d3223f0bd2f510142a01f4d88ad34254aa
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-04-05 15:22:09 +0200

    Main file for Zone dw
    This file is the main file of Zone Data warehouse,
    it calls all other files in Zone DW

---
+++ DW/f_dw.sql
@@ -0,0 +1,16 @@
+--CREATE Dimension catalogue
+
+@f_dw_catalogue.sql;
+
+--CREATE Dimension date
+
+@f_dw_date.sql;
+
+--CREATE Dimension magasin
+
+@f_dw_magasin.sql;
+
+--CREATE "Table de fait"
+
+@f_dw_vente.sql;
+
