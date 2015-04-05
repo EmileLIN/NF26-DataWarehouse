ec3b938588bd96d8e88852c380fc1b670cfabc4e
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-04-05 15:20:37 +0200

    Table f_dw_catalogue builder
    This file allows to build table f_dw_catalogue

---
+++ DW/f_dw_catalogue.sql
@@ -0,0 +1,16 @@
+--Create the object f_dw_catalogue of catalogue_type 
+
+CREATE TABLE f_dw_catalogue(
+  ISBN          CHAR(13),
+  AUTEUR        VARCHAR(255),
+  LANGUE        CHAR(3),
+  PUBLICATION   VARCHAR(50),
+  EDITEUR       VARCHAR(255)
+);
+
+CREATE UNIQUE INDEX f_dw_catalogue_index ON f_dw_catalogue(ISBN);
+
+ALTER TABLE f_dw_catalogue
+  ADD CONSTRAINT pk_f_dw_catalogue PRIMARY KEY (ISBN); 
+
+  
