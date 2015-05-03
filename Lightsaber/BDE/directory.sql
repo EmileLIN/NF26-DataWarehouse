cb4329fa53fcc75dca515ef36864bd05a517da41
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-05-03 10:57:49 +0200

    BDE directory constructor

---
+++ Lightsaber/BDE/directory.sql
@@ -0,0 +1,5 @@
+--Create Directories
+
+CREATE OR REPLACE DIRECTORY Lightsaber_src_directory AS '/home/nf26/sw/0/';
+CREATE OR REPLACE DIRECTORY Lightsaber_log_directory AS '/volsme/users/nf26p018/lightsaber/log/';
+
