4325ff6ae6f98306ae4c8a28721dbc90f5cb3a71
EmileLIN <shuhanLin@shuhantekimacbook-pro.home>
2015-05-03 10:58:04 +0200

    BDE main file

---
+++ Lightsaber/BDE/f_bde.sql
@@ -0,0 +1,11 @@
+@./BDE/directory.sql
+
+@./BDE/f_bde_clerk.sql
+@./BDE/f_bde_factory.sql
+@./BDE/f_bde_product.sql
+@./BDE/f_bde_species.sql
+@./BDE/f_bde_store.sql
+@./BDE/f_bde_customer.sql
+
+@./BDE/f_bde_lightsaberInc.sql
+
